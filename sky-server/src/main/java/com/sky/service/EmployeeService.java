e1887d02d1e3abf0320e0ff770f091e8f5b12a1b
Hope <1556802273@qq.com>
2024-02-07 14:49:40 +0800

    修改员工信息

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -23,4 +23,8 @@ public interface EmployeeService {
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    Employee getById(Long id);
+
+    void update(EmployeeDTO employeeDTO);
 }
