5d3df349692816aa173f8323e1c4ef208eedee28
Hope <1556802273@qq.com>
2024-02-24 09:41:28 +0800

    新增套餐

---
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -0,0 +1,17 @@
+package com.sky.service;
+
+import com.sky.dto.SetmealDTO;
+
+/**
+ * ClassName: SetmealService
+ * Package: com.sky.service
+ * Description:
+ *
+ * @Author han
+ * @Create 2024/2/24  8:46
+ * Version 1.0
+ */
+public interface SetmealService {
+
+    void save(SetmealDTO setmealDTO);
+}
